911ce7d02497331429edaa6a699adf177dedf177
Damien Weatherley <damien.weatherley@hotmail.com>
2021-06-28 14:46:36 +0100

    Modify dinosuar fact object to an array. Add forEach methods to the compare methods and push new strings to dinosaur object fact arrays and use these randomly in the generateTiles function. Remove paragraph element from human tile.  Fix humanInfo diet string to lowercase to fix diet compare function.  Remove console.logs.

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-
-    // Create Dino Constructor
+// Create Dino Constructor
 function Dino(name, weight, height, diet, where, when, fact) {
   this.species = name;
   this.weight = weight;
@@ -7,20 +6,20 @@ function Dino(name, weight, height, diet, where, when, fact) {
   this.diet = diet;
   this.where = where;
   this.when = when;
-  this.fact = fact;
+  this.fact = [fact];
 }
 
-    // Create Dino Objects
+// Create Dino Objects
 const triceratops = new Dino("Triceratops", 13000, 114, "herbavor", "North America", "Late Cretaceous", "First discovered in 1889 by Otheniel Charles Marsh");
 
 const tRex = new Dino("Tyrannosaurus Rex", 11905, 144,
-"carnivor", "North America", "Late Cretaceous", "The largest known skull measures in at 5 feet long.");
+  "carnivor", "North America", "Late Cretaceous", "The largest known skull measures in at 5 feet long.");
 
 const anklyo = new Dino("Anklyosaurus", 10500, 55, "herbavor", "North America", "Late Cretaceous", "Anklyosaurus survived for approximately 135 million years.");
 
-const brachio = new Dino ("Brachiosaurus", 70000, 372, "70000", "North America", "Late Jurasic", "An asteroid was named 9954 Brachiosaurus in 1991.");
+const brachio = new Dino("Brachiosaurus", 70000, 372, "70000", "North America", "Late Jurasic", "An asteroid was named 9954 Brachiosaurus in 1991.");
 
-const steg = new Dino("Stegosaurus", 11600, 79, "herbavor", "North America, Europe, Asia",  "Late Jurasic to Early Cretaceous", "The Stegosaurus had between 17 and 22 seperate places and flat spines.");
+const steg = new Dino("Stegosaurus", 11600, 79, "herbavor", "North America, Europe, Asia", "Late Jurasic to Early Cretaceous", "The Stegosaurus had between 17 and 22 seperate places and flat spines.");
 
 const elasmo = new Dino("Elasmosaurus", 16000, 59, "carnivor", "North America", "Late Cretaceous", "Elasmosaurus was a marine reptile first discovered in Kansas.");
 
@@ -36,157 +35,155 @@ const dinos = [triceratops, tRex, anklyo, brachio, steg, elasmo, ptera, pigeon];
 
 const shuffledDinos = dinos.sort(() => 0.5 - Math.random());
 
-    // Create Human Object
+// Create Human Object
 
-    var humanInfo = {};
+let humanInfo = {};
 
-    // Use IIFE to get human data from form
+// Use IIFE to get human data from form
 
-    const addHuman = (event)=>{
-      event.preventDefault();
-      let human = {
-        name: document.getElementById("name").value,
-        height: parseInt(((document.getElementById("feet").value) * 12)) + parseInt(document.getElementById("inches").value),
-        weight: parseInt(document.getElementById("weight").value),
-        diet: document.getElementById("diet").value
-      }
-        humanInfo = human;
-      console.log("added", humanInfo);
-    };
+const addHuman = (event) => {
+  event.preventDefault();
+  let humanDiet = document.getElementById("diet").value;
+  let human = {
+    name: document.getElementById("name").value,
+    height: parseInt(((document.getElementById("feet").value) * 12)) + parseInt(document.getElementById("inches").value),
+    weight: parseInt(document.getElementById("weight").value),
+    diet: humanDiet.toLowerCase()
+  }
+  humanInfo = human;
+};
 
-      var btn = document.getElementById("btn");
-      btn.addEventListener("click", addHuman);
+const btn = document.getElementById("btn");
+btn.addEventListener("click", addHuman);
 
-    //Random number 0 - 7
+// Create Dino Compare Method 1 - comparing height
+// NOTE: Weight in JSON file is in lbs, height in inches.
 
-    var ranNum = function randomNumber0to7() {
-      return(Math.floor(Math.random() * 7));
-      };
-      // var ranNum = randomNumber0to7();
+let compHeight = "";
 
-    // Create Dino Compare Method 1
-    // NOTE: Weight in JSON file is in lbs, height in inches.
-    const dinoNum = ranNum();
-
-    var compHeight = "";
-    function compareHeight() {
-      console.log(dinos[dinoNum]);
-      if (humanInfo.height > dinos[dinoNum].height) {
-        compHeight = "You are taller than a " + dinos[dinoNum].species;
-        console.log(compHeight);
-      } else {
-        compHeight = "You are smaller than a " + dinos[dinoNum].species;
-        console.log(compHeight);
-      }
+function compareHeight() {
+  dinos.forEach((dino, index) => {
+    if (humanInfo.height > dinos[index].height) {
+      compHeight = "You are taller than a " + dinos[index].species;
+      dinos[index].fact.push(compHeight);
+    } else {
+      compHeight = "You are smaller than a " + dinos[index].species;
+      dinos[index].fact.push(compHeight);
     }
+  })
+};
 
-      btn.addEventListener("click", compareHeight);
+btn.addEventListener("click", compareHeight);
 
-    // Create Dino Compare Method 2
-    // NOTE: Weight in JSON file is in lbs, height in inches.
-    var compWeight = "";
-    function compareWeight() {
-      console.log(dinos[dinoNum]);
-      if (humanInfo.weight > dinos[dinoNum].weight) {
-        compWeight = "heavier than a " + dinos[dinoNum].species;
-        console.log(compWeight);
-      } else {
-        compWeight = "lighter than a " + dinos[dinoNum].species;
-        console.log(compWeight);
-      }
+// Create Dino Compare Method 2 - comparing weight
+// NOTE: Weight in JSON file is in lbs, height in inches.
+
+let compWeight = "";
+
+function compareWeight() {
+  dinos.forEach((dino, index) => {
+    if (humanInfo.weight > dinos[index].weight) {
+      compWeight = "You are heavier than a " + dinos[index].species;
+      dinos[index].fact.push(compWeight);
+    } else {
+      compWeight = "You are lighter than a " + dinos[index].species;
+      dinos[index].fact.push(compWeight);
     }
+  })
+};
 
-      btn.addEventListener("click", compareWeight);
+btn.addEventListener("click", compareWeight);
 
-    // Create Dino Compare Method 3
-    // NOTE: Weight in JSON file is in lbs, height in inches.
-    var compDiet = "";
-    function compareDiet() {
-      console.log(dinos[dinoNum]);
-      if (humanInfo.diet === dinos[dinoNum].diet) {
-        compDiet = "and eat the same food as a " + dinos[dinoNum].species;
-        console.log(compDiet);
-      } else {
-        compDiet = "but eat different food to a " + dinos[dinoNum].species;
-        console.log(compDiet);
-      }
+// Create Dino Compare Method 3 - comparing diet
+
+let compDiet = "";
+
+function compareDiet() {
+  dinos.forEach((dino, index) => {
+    if (humanInfo.diet === dinos[index].diet) {
+      compDiet = "You eat the same food as a " + dinos[index].species;
+      dinos[index].fact.push(compDiet);
+    } else {
+      compDiet = "You eat different food to a " + dinos[index].species;
+      dinos[index].fact.push(compDiet);
     }
+  });
+}
 
-      btn.addEventListener("click", compareDiet);
+btn.addEventListener("click", compareDiet);
 
-    // Generate Tiles for each Dino in Array
+//Random number 0 - 3 to use in selecting random dino fact
 
-    function addTiles() {
-      //creates html elements
-      let newDiv = document.createElement("div");
-      let newH3 = document.createElement("H3");
-      let newImg = document.createElement("img");
-      let newP = document.createElement("p");
+let ranNum = function randomNumber0to7() {
+  return (Math.floor(Math.random() * 4));
+};
 
-      //creates value of the class and id attribute to the html elements
-      newDiv.className = "grid-item";
-      newDiv.id = i;
+// Generate Tiles for each Dino in Array
 
-      //sets the source of the image
-      const dinoName = shuffledDinos[i].species;
-      newImg.src = "images/" + dinoName.toLowerCase() + ".png";
+function generateTiles() {
+  //Generates random number for getting random dinosaur text content
+  randomFactIndex = ranNum();
 
-      //sets the text content
-      newH3.innerText = shuffledDinos[i].species;
-      newP.innerText = shuffledDinos[i].fact;
+  //creates html elements
+  let newDiv = document.createElement("div");
+  let newH3 = document.createElement("H3");
+  let newImg = document.createElement("img");
+  let newP = document.createElement("p");
 
-      //positions the new elements in the html document
-      let position = document.getElementById("grid");
+  //creates value of the class and id attribute to the html elements
+  newDiv.className = "grid-item";
+  newDiv.id = i;
 
-      position.appendChild(newDiv);
-      newDiv.append(newH3, newImg, newP);
-    }
+  //sets the source of the image
+  const dinoName = shuffledDinos[i].species;
+  newImg.src = "images/" + dinoName.toLowerCase() + ".png";
 
-        // Add tiles to DOM
-        for(i=0; i<shuffledDinos.length; i++){
-       addTiles(shuffledDinos[i]);
-     }
+  //sets the text content
+  newH3.innerText = shuffledDinos[i].species;
+  newP.innerText = shuffledDinos[i].fact[randomFactIndex];
 
-     //Insert human tile to centre of grid
-     function humanCentre(){
-       let newDiv = document.createElement("div");
-       let newH3 = document.createElement("H3");
-       let newImg = document.createElement("img");
-       let newP = document.createElement("p");
-       let grid = document.getElementById("grid");
+  //positions the new elements in the html document
+  let position = document.getElementById("grid");
 
-       newDiv.className = "grid-item";
-       newImg.src = "images/human.png";
-       newH3.innerText = humanInfo.name;
-       newP.innerText = compHeight + ", " + compWeight + " " + compDiet;
+  position.appendChild(newDiv);
+  newDiv.append(newH3, newImg, newP);
+}
 
-       let centre = document.getElementById("4")
-       grid.insertBefore(newDiv, centre);
-       newDiv.append(newH3, newImg, newP);
-     }
+// Add tiles to DOM
+function addTiles() {
+  for (i = 0; i < shuffledDinos.length; i++) {
+    generateTiles(shuffledDinos[i]);
+  }
+}
 
-     btn.addEventListener("click", humanCentre);
+btn.addEventListener("click", addTiles);
 
-    // Remove form from screen and display grid
+//Insert human tile to centre of grid
+function humanCentre() {
+  let newDiv = document.createElement("div");
+  let newH3 = document.createElement("H3");
+  let newImg = document.createElement("img");
+  let grid = document.getElementById("grid");
 
-    function removeFormAddGrid() {
-      document.getElementById("dino-compare").style.visibility = "hidden";
-      document.getElementById("dino-compare").style.position = "absolute";
-      document.getElementById("grid").style.visibility = "visible";
-      document.getElementById("grid").style.position = "";
-    }
+  newDiv.className = "grid-item";
+  newImg.src = "images/human.png";
+  newH3.innerText = humanInfo.name;
 
-      btn.addEventListener("click", removeFormAddGrid);
+  let centre = document.getElementById("4")
+  grid.insertBefore(newDiv, centre);
+  newDiv.append(newH3, newImg);
+}
 
+btn.addEventListener("click", humanCentre);
 
-    // On button click, prepare and display infographic
+// Remove form from screen and display grid
 
-    // function displayInfographic(){
-    //   addHuman();
-    //   compareHeight();
-    //   compWeight();
-    //   compDiet();
-    //   humanCentre();
-    //   removeFormAddGrid();
-    // }
-    // btn.addEventListener("click", displayInfographic);
+function removeFormAddGrid() {
+  document.getElementById("dino-compare").style.visibility = "hidden";
+  document.getElementById("dino-compare").style.position = "absolute";
+  document.getElementById("grid").style.visibility = "visible";
+  document.getElementById("grid").style.position = "";
+}
+// On button click, prepare and display infographic
+
+btn.addEventListener("click", removeFormAddGrid);
